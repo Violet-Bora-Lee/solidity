f71e542f9a1e0c57be4c1bad82ccf05df6143b62
Matheus Aguiar <matheus.pit@gmail.com>
2022-06-20 19:19:20 -0300

    Added overflow checks after multiplication operation is executed.

--- test/formal/checked_uint_mul_16.py
+++ test/formal/checked_uint_mul_16.py
@@ -1,4 +1,4 @@
-from opcodes import AND, ISZERO, GT, DIV
+from opcodes import AND, ISZERO, GT, DIV, MUL
 from rule import Rule
 from util import BVUnsignedUpCast, BVUnsignedMax
 from z3 import BitVec, Not, BVMulNoOverflow
@@ -9,9 +9,8 @@ Overflow checked unsigned integer multiplication.
 
 # Approximation with 16-bit base types.
 n_bits = 16
-type_bits = 8
 
-while type_bits <= n_bits:
+for type_bits in [4, 8, 12, 16]:
 
 	rule = Rule()
 
@@ -25,13 +24,14 @@ while type_bits <= n_bits:
 	# cast to full n_bits values
 	X = BVUnsignedUpCast(X_short, n_bits)
 	Y = BVUnsignedUpCast(Y_short, n_bits)
+	product = MUL(X, Y)
 
 	# Constants
 	maxValue = BVUnsignedMax(type_bits, n_bits)
 
-	# Overflow check in YulUtilFunction::overflowCheckedIntMulFunction
-	overflow_check = AND(ISZERO(ISZERO(X)), GT(Y, DIV(maxValue, X)))
-
+	# Overflow check in YulUtilFunction::overflowCheckedIntMulFunctions
+	if type_bits > n_bits / 2:
+		overflow_check = AND(ISZERO(ISZERO(X)), GT(Y, DIV(maxValue, X)))
+	else:
+		overflow_check = GT(product, maxValue)
 	rule.check(overflow_check != 0, actual_overflow)
-
-	type_bits *= 2
